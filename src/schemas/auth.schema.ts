78983fb40250460b1586c5e22d6a8168e860d628
Can Nam Khanh <khanhcnf8@fullstack.edu.vn>
2026-06-10 04:00:37 +0700

    update

--- src/schemas/auth.schema.ts
+++ src/schemas/auth.schema.ts
@@ -15,9 +15,7 @@ export type RegisterInput = z.infer<typeof registerSchema>;
 export type LoginInput = z.infer<typeof loginSchema>;
 
 export const refreshTokenSchema = z.object({
-  body: z.object({
-    refreshToken: z.string().min(1, { message: "Refresh token is required" }),
-  }),
+  refreshToken: z.string().min(1, { message: "Refresh token is required" }),
 });
 
 export type RefreshTokenInput = z.infer<typeof refreshTokenSchema>;
